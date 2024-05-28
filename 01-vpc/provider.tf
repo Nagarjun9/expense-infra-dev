06a83e747a6d0259485c9900179dde084a8109c3
Nagarjun9 <nagunaggu@gmail.com>
2024-05-28 21:59:13 +0530

    vpv

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -1,20 +1,21 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hasicorp/aws"
-            version = "5.48.0"
-        }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
     }
-    backend "s3" {
-      bucket = "nagarjuna9-remote-state"
-      key = "expense-vpc"
-      region = "us-east-1"
-      dynamodb_table = "nagarjuna9-locking"
-
-   }
-}
-
-provider "aws" {
+  }
+  backend "s3" {
+    bucket = "nagarjuna9-remote-state"
+    key    = "expense-vpc"
     region = "us-east-1"
+    dynamodb_table = "nagarjuna9-locking"
+  }
 }
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
+}
+
 
