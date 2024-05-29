f8dfd208587a5882060b3b51a92b64cee15909a9
Nagarjun9 <nagunaggu@gmail.com>
2024-05-29 21:19:11 +0530

    vpc

--- 03-bastion/main.tf
+++ 03-bastion/main.tf
@@ -3,7 +3,7 @@ module "bastion" {
 
     name = "${var.project_name}-${var.environment}-bastion"
     instance_type = t3.micro
-    vpn_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
+    vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
     subnet_id = local.public_subnet_id 
     ami = data.aws_ami.ami_info.id 
     user_data = file("bastion.sh")
