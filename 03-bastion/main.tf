0ddbeba4f9ec14964334ff074b6832752a587128
Nagarjun9 <nagunaggu@gmail.com>
2024-05-29 21:16:09 +0530

    vpc

--- 03-bastion/main.tf
+++ 03-bastion/main.tf
@@ -1,5 +1,5 @@
 module "bastion" {
-    source = "terraform-aws-modules/ec2-instance/aws"
+  source  = "terraform-aws-modules/ec2-instance/aws"
 
     name = "${var.project_name}-${var.environment}-bastion"
     instance_type = t3.micro
