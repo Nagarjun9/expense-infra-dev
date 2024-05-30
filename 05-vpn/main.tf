e4ad498cb79227d05ec6a04d1a76a1b22a5b4faa
Nagarjun9 <nagunaggu@gmail.com>
2024-05-30 21:54:01 +0530

    vpc

--- 05-vpn/main.tf
+++ 05-vpn/main.tf
@@ -8,15 +8,16 @@ module "vpn" {
     key_name = aws_key_pair.vpn.key_name
     name = "${var.project_name}-${var.environment}-vpn"
 
-    instance_type  = "t3.micro"
-    vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
-    subnet_id = local.public_subnet_id
-    ami = data.aws_ami.ami_info.id 
-
-    tags = merge (
-        var.common_tags,
-        {
-            name = "${var.project_name}-${var.environment}-vpn"
-        }
-    )
+    instance_type          = "t3.micro"
+  vpc_security_group_ids = [data.aws_ssm_parameter.vpn_sg_id.value]
+  # convert StringList to list and get first element
+  subnet_id = local.public_subnet_id
+  ami = data.aws_ami.ami_info.id
+  
+  tags = merge(
+    var.common_tags,
+    {
+        Name = "${var.project_name}-${var.environment}-vpn"
+    }
+  )
 }
